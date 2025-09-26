bfa3ccc4922db91a91b5ec09940b68f3d555e395
zfliex <zfliex924@gmail.com>
2025-09-26 14:26:41 +0800

    test upgrade

--- params/config.go
+++ params/config.go
@@ -212,12 +212,12 @@ var (
 		TheseusTime:         newUint64(1748592000), // 2025-05-30 08:00:00 AM UTC
 		CancunTime:          newUint64(1748592000), // 2025-05-30 08:00:00 AM UTC
 		TheseusFixTime:      newUint64(1752652800), // 2025-07-16 08:00:00 AM UTC
-		LubanTime:           newUint64(1758700800),
-		PlatoTime:           newUint64(1758700800),
-		BohrTime:            newUint64(1758700800),
-		PascalTime:          newUint64(1758700800),
-		PragueTime:          newUint64(1758700800),
-		HermesTime:          newUint64(1758700800), // 2025-09-24 08:00:00 AM UTC
+		LubanTime:           newUint64(1758868800),
+		PlatoTime:           newUint64(1758868800),
+		BohrTime:            newUint64(1758868800),
+		PascalTime:          newUint64(1758868800),
+		PragueTime:          newUint64(1758868800),
+		HermesTime:          newUint64(1758868800),
 		Satoshi: &SatoshiConfig{
 			Period: 3,
 			Epoch:  200,
